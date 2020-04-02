889bd3d8247999fbac6835b89ecb065398982d54
Victor Pereira de Paula <victorpereiradepaula@gmail.com>
2020-04-01 22:54:49 -0400

    removing a 'ghost' imageView back to headerView on login screen

---
+++ empresas-ios/features/enterpriseSearch/enterpriseCell/EnterpriseCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  EnterpriseCellViewModel.swift
+//  empresas-ios
+//
+//  Created by Victor Pereira on 01/04/20.
+//  Copyright © 2020 Victor Pereira. All rights reserved.
+//
+
+import Foundation
+
+final class EnterpriseCellViewModel {
+    
+}
